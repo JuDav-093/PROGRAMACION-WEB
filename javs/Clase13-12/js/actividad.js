2b15a2f77970ec967477d77ebe55912cd543c189
Anabel Davila <juanislala2@gmail.com>
2025-01-06 12:37:45 -0500

    actividad

---
+++ javs/Clase13-12/js/actividad.js
@@ -0,0 +1,39 @@
+// Función declarada: calcularPromedio
+function calcularPromedio(a, b, c) {
+    if (![a, b, c].every(num => typeof num === 'number')) {
+      console.log("Solo Puede ingresar números.");
+      return;
+    }
+    return (a + b + c) / 3;
+  }
+  
+  // Función expresada: determinarMayor
+  const determinarMayor = function(a, b) {
+    if (![a, b].every(num => typeof num === 'number')) {
+      console.log("Ambos parámetros deben ser números.");
+      return;
+    }
+    return a > b ? a : b;
+  };
+  
+  // Función flecha: esPar
+  const esPar = num => {
+    if (typeof num !== 'number') {
+      console.log("Solo se puede ingresar números.");
+      return;
+    }
+    return num % 2 === 0;
+  };
+  
+  //funcion autonoma
+  (() => {
+    try {
+      const [num1, num2, num3] = [10, 15, 20];
+      console.log(`Promedio: ${calcularPromedio(num1, num2, num3)}`);
+      console.log(`Mayor: ${determinarMayor(num1, num2)}`);
+      console.log(`${num1} es ${esPar(num1) ? "par" : "impar"}`);
+    } catch (error) {
+      console.error(error.message);
+    }
+  })();
+  
